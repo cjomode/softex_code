49aeec2e03f08ffd8a53c28a6692febb33ef25d4
Juliana Moreira <oliveiramjuliana22@gmail.com>
2022-08-27 18:34:26 -0300

    atividades

---
+++ atvd18.py
@@ -0,0 +1,14 @@
+anoCorreto = False
+nome = input('Digite seu nome completo: ')
+while(anoCorreto == False):
+    try:
+        anoNas = int(input('Digite seu ano de nascimento: '))
+        if(anoNas >= 1922) and (anoNas <= 2021):
+            anoCorreto = True
+            idade = 2022 - anoNas
+            print('Seu nome é:',nome)
+            print('Sua idade é:',idade)
+        else:
+            print('Digite um ano válido (entre 1922 e 2021)!\n')
+    except:
+        print('Digite um número!\n') 
